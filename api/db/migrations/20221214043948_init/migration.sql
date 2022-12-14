b063e1b27bab222f1007e878e8447b05e396dd7e
pi0neerpat <blockchainbuddha@gmail.com>
2022-12-13 20:40:56 -0800

    supabase setup

--- api/db/migrations/20221214043948_init/migration.sql
+++ api/db/migrations/20221214043948_init/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "username" TEXT,
     "address" TEXT,
     "locale" TEXT,
     "email" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "mfa_enabled" BOOLEAN,
     "imageSrc" TEXT,
     "country" TEXT,
@@ -15,17 +15,20 @@ CREATE TABLE "User" (
     "blocked" BOOLEAN,
     "refreshToken" TEXT,
     "accessToken" TEXT,
-    "betaAccess" BOOLEAN NOT NULL DEFAULT false
+    "betaAccess" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "OAuth" (
-    "state" TEXT NOT NULL PRIMARY KEY,
+    "state" TEXT NOT NULL,
     "codeChallenge" TEXT NOT NULL,
     "codeVerifier" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "userId" TEXT,
-    CONSTRAINT "OAuth_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+    CONSTRAINT "OAuth_pkey" PRIMARY KEY ("state")
 );
 
 -- CreateIndex
@@ -36,3 +39,6 @@ CREATE UNIQUE INDEX "User_address_key" ON "User"("address");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "OAuth" ADD CONSTRAINT "OAuth_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
